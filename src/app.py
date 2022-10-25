a074239360787a73880d5003c739fa4b6a781ad9
Javierklie <javierklie1@gmail.com>
2022-10-25 01:24:24 +0000

    first commit

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, json, request
+app = Flask(__name__)
+
+todos = [{ "label": "My first task", "done": False }]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_tras = jsonify(todos)
+    return json_tras
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request.data)
+    todos.append(decoded_object)
+    json_todos = jsonify(todos)
+    return json_todos
+    print("Incoming request with the following body", request_body)
+    return 'Response for the POST todo'
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    json_newtodos = jsonify(todos)
+    return json_newtodos
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
+
+
+
+
